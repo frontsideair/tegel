260bdcad95e64527df15012a22369348b7877d6b
Fatih Altinok <photonia@gmail.com>
2019-09-11 11:38:56 +0300

    Add basic functionality test

---
+++ tests/index.test.js
@@ -0,0 +1,19 @@
+const request = require("supertest");
+const app = require("../example/app");
+
+test("It should read hello world", async () => {
+  const response = await request(app).get("/");
+  expect(response.text).toBe("Hello, world!");
+});
+
+test("It should switch universe flag on", async () => {
+  const response = await request(app)
+    .post("/tgl")
+    .send("universal=on");
+  expect(response.status).toBe(200);
+});
+
+test("It should read hello universe", async () => {
+  const response = await request(app).get("/");
+  expect(response.text).toBe("Hello, universe!");
+});
