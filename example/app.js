d5237f2795d792ec40484929ce99ba986420674d
Fatih Altinok <fatihaltinok@live.com>
2019-11-30 02:57:09 +0300

    Cached object API (#18)
    * Initial work on cached object API
    
    * Fix test
    
    * Replace Objec.fromEntries for better compatibility
    
    * Refactor
    
    * Refactor
    
    * Even more refactor

--- example/app.js
+++ example/app.js
@@ -1,31 +1,31 @@
 const express = require("express");
 const Keyv = require("keyv");
-const Tgl = require("../lib");
+const { Tgl, middleware, router } = require("../lib");
 
 const toggles = [
   {
     name: "universal",
     description: "Do we address the world, or the whole universe?",
-    default: false
+    defaultValue: false
   },
   {
     name: "lasers",
     description: "Enable lasers!!!",
-    default: true
+    defaultValue: true
   }
 ];
-const storage = new Keyv("sqlite://:memory:");
+const storage = new Keyv();
 const tgl = new Tgl({ toggles, storage });
 
 const app = express();
 
-app.use(tgl.middleware());
+app.use(middleware(tgl));
 
-app.get("/", async (req, res) => {
-  const universal = await req.tgl.get("universal");
+app.get("/", (req, res) => {
+  const universal = req.tgl.universal;
   res.send(universal ? "Hello, universe!" : "Hello, world!");
 });
 
-app.use("/tgl", tgl.router());
+app.use("/tgl", router(tgl));
 
 module.exports = app;
